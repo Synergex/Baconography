01cf123cfb10ce3f456e67bdda971d96d1bbbc5f
Marty Lewis <marty.lewis@synergex.com>
2013-02-27 10:33:28 -0800

    Initial impl for markdown button

---
+++ BaconographyWP8/Common/MarkdownButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BaconographyWP8.Common
+{
+	class MarkdownButton : Button
+	{
+		public static readonly DependencyProperty UrlProperty =
+			DependencyProperty.Register(
+				"Url",
+				typeof(object),
+				typeof(FixedLongListSelector),
+				new PropertyMetadata(null, OnUrlChanged)
+			);
+
+		public object Url
+		{
+			get { return GetValue(UrlProperty); }
+			set { SetValue(UrlProperty, value); }
+		}
+
+		private static void OnUrlChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			var button = (MarkdownButton)d;
+			button.Url = e.NewValue;
+		}
+	}
+}
