947c908b52fae92988820e46984732bc1712855c
Marty Lewis <marty.lewis@synergex.com>
2013-03-01 12:39:19 -0800

    Early commit for login

---
+++ BaconographyWP8/View/LoginView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace BaconographyWP8.View
+{
+    public sealed partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            this.InitializeComponent();
+        }
+
+        //hack for visual state managment so we can watermark the passwordbox
+        private void PasswordBox_GotFocus_1(object sender, RoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(passwordBox, "WatermarkHidden", true);
+        }
+
+        private void PasswordBox_LostFocus_1(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(passwordBox.Password))
+            {
+                VisualStateManager.GoToState(passwordBox, "WatermarkVisible", true);
+            }
+        }
+
+        private void passwordBox_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(passwordBox.Password))
+            {
+                VisualStateManager.GoToState(passwordBox, "WatermarkVisible", true);
+            }
+        }
+    }
+}
