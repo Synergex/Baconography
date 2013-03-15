c25f3576679ba43f05a6c243efc5a88e3b0737c2
Marty Lewis <marty.lewis@synergex.com>
2013-03-15 08:57:31 -0700

    Tweaked upvote color

--- BaconographyWP8/Converters/VoteColorConverter.cs
+++ BaconographyWP8/Converters/VoteColorConverter.cs
@@ -15,7 +15,7 @@ namespace BaconographyWP8.Converters
     public class VoteColorConverter : IValueConverter
     {
 		static SolidColorBrush neutral = new SolidColorBrush(System.Windows.Media.Colors.White);
-		static SolidColorBrush upvote = new SolidColorBrush(System.Windows.Media.Color.FromArgb(255, 255, 103, 00));
+		static SolidColorBrush upvote = new SolidColorBrush(System.Windows.Media.Color.FromArgb(255, 255, 154, 00));
 		static SolidColorBrush downvote = new SolidColorBrush(System.Windows.Media.Color.FromArgb(255, 85, 77, 216));
 
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
