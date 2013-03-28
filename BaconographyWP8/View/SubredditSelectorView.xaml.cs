ffb096c3c089461365d9310bd1235ab4ebce980e
Marty Lewis <marty.lewis@synergex.com>
2013-03-27 17:23:10 -0700

    Enter key on keyboard toggles submit button

--- BaconographyWP8/View/SubredditSelectorView.xaml.cs
+++ BaconographyWP8/View/SubredditSelectorView.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using BaconographyPortable.ViewModel;
 
 namespace BaconographyWP8.View
 {
@@ -15,6 +16,17 @@ namespace BaconographyWP8.View
 		public SubredditSelectorView()
 		{
 			this.InitializeComponent();
+		}
+
+		private void manualBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+		{
+			if (e.Key == System.Windows.Input.Key.Enter)
+			{
+				this.Focus();
+				var ssvm = this.DataContext as SubredditSelectorViewModel;
+				if (ssvm != null)
+					ssvm.SubmitSubreddit.Execute(ssvm);
+			}
 		}
 	}
 }
