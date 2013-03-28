f733fc82dfd1870081a58df59c3a86e8d6eb5899
Jeff Greene <hippiehunterenator@gmail.com>
2013-03-28 10:17:02 -0700

    changed from LZMA to LZ4, our content type compresses just as well with LZ4 but is several times faster and uses significantly less memory

--- BaconographyPortable/Model/Compression/CompressionService.cs
+++ BaconographyPortable/Model/Compression/CompressionService.cs
@@ -1,5 +1,4 @@
 ﻿using BaconographyPortable.Services;
-using SevenZip;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,75 +10,46 @@ namespace BaconographyPortable.Model.Compression
 {
     public class CompressionService : ICompressionService
     {
-        public byte[] Compress(byte[] bytes)
+        public unsafe byte[] Compress(byte[] bytes)
         {
-            string mf = "bt4";
-            Int32 dictionary = 1 << 23;
-            Int32 posStateBits = 2;
-            Int32 litContextBits = 2; // for normal files
-            // UInt32 litContextBits = 0; // for 32-bit data
-            Int32 litPosBits = 0;
-            // UInt32 litPosBits = 2; // for 32-bit data
-            Int32 algorithm = 2;
-            Int32 numFastBytes = 128;
-            CoderPropID[] propIDs = 
-                {
-                    CoderPropID.DictionarySize,
-                    CoderPropID.PosStateBits,
-                    CoderPropID.LitContextBits,
-                    CoderPropID.LitPosBits,
-                    CoderPropID.Algorithm,
-                    CoderPropID.NumFastBytes,
-                    CoderPropID.MatchFinder,
-                    CoderPropID.EndMarker
-                };
-            object[] properties = 
-                {
-                    (Int32)(dictionary),
-                    (Int32)(posStateBits),
-                    (Int32)(litContextBits),
-                    (Int32)(litPosBits),
-                    (Int32)(algorithm),
-                    (Int32)(numFastBytes),
-                    mf,
-                    true
-                };
-            var encoder = new SevenZip.Compression.LZMA.Encoder();
-            encoder.SetCoderProperties(propIDs, properties);
+            var result = new byte[LZ4n.LZ4Codec.MaximumOutputLength(bytes.Length) + 4];
+            var length = LZ4n.LZ4Codec.Encode32(bytes, 0, bytes.Length, result, 4, result.Length - 4);
 
-            var inStream = new MemoryStream(bytes);
-            var outStream = new MemoryStream();
-            encoder.WriteCoderProperties(outStream);
-            for (int i = 0; i < 8; i++)
-                outStream.WriteByte((Byte)(bytes.Length >> (8 * i)));
-            encoder.Code(inStream, outStream, bytes.Length, -1, null);
-            return outStream.ToArray();
+            if (length != result.Length)
+            {
+                if (length < 0)
+                    throw new InvalidOperationException("Compression has been corrupted");
+                var buffer = new byte[length + 4];
+                Buffer.BlockCopy(result, 4, buffer, 4, length);
+                fixed (byte* bytesPtr = buffer)
+                {
+                    *((int*)bytesPtr) = bytes.Length;
+                }
+                return buffer;
+            }
+            else if ((length + 4) == result.Length)
+            {
+                fixed (byte* bytesPtr = result)
+                {
+                    *((int*)bytesPtr) = bytes.Length;
+                }
+                return result;
+            }
+            else
+                throw new InvalidOperationException("Compression has been corrupted");
         }
 
-        public byte[] Decompress(byte[] bytes)
+        public unsafe byte[] Decompress(byte[] bytes)
         {
-            SevenZip.Compression.LZMA.Decoder decoder;
-            decoder = new SevenZip.Compression.LZMA.Decoder();
-            var inStream = new MemoryStream(bytes);
-            var outStream = new MemoryStream();
-            byte[] properties = new byte[5];
-            if (inStream.Read(properties, 0, 5) != 5)
-                return null;
-            decoder.SetDecoderProperties(properties);
+            if (bytes == null || bytes.Length < 4)
+                return new byte[0];
 
-            long outSize = 0;
-            for (int i = 0; i < 8; i++)
+            int decompressedSize = 0;
+            fixed (byte* bytesPtr = bytes)
             {
-                int v = inStream.ReadByte();
-                if (v < 0)
-                    throw (new Exception("Can't Read 1"));
-                outSize |= ((long)(byte)v) << (8 * i);
+                decompressedSize = *((int*)bytesPtr);
             }
-            long compressedSize = inStream.Length - inStream.Position;
-
-
-            decoder.Code(inStream, outStream, compressedSize, outSize, null);
-            return outStream.ToArray();
+            return LZ4n.LZ4Codec.Decode32(bytes, 4, bytes.Length - 4, decompressedSize);
         }
     }
 }
