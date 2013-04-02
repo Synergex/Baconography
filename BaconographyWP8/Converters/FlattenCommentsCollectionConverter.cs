63fc7f3b5535d563be1c0cd0ac1d0a60bc7f8725
Jeff Greene <hippiehunterenator@gmail.com>
2013-04-02 15:31:31 -0700

    dont treat static converter like its instance

--- BaconographyWP8/Converters/FlattenCommentsCollectionConverter.cs
+++ BaconographyWP8/Converters/FlattenCommentsCollectionConverter.cs
@@ -18,115 +18,124 @@ namespace BaconographyWP8.Converters
 {
 	public class FlattenCommentsCollectionConverter : IValueConverter
     {
-		private ObservableCollection<ViewModelBase> Comments;
-		private CommentViewModelCollection originalCollection;
-
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-			originalCollection = value as CommentViewModelCollection;
-			originalCollection.CollectionChanged += Comment_CollectionChanged;
-			Comments = new ObservableCollection<ViewModelBase>();
-			foreach (var child in originalCollection)
-			{
-				VisitAddChildren(child);
-				
-			}
-
-			return Comments;
+            return new FlattenCommentsCollection(value as CommentViewModelCollection);
         }
 
-		private void VisitAddChildren(ViewModelBase vm, int index = -1)
-		{
-			if (index < 0)
-				Comments.Add(vm);
-			else
-				Comments.Insert(index, vm);
+        private class FlattenCommentsCollection : ObservableCollection<ViewModelBase>
+        {
+            public FlattenCommentsCollection(CommentViewModelCollection originalCollection)
+            {
+                _originalCollection = originalCollection;
+                if (_originalCollection != null)
+                {
+                    _originalCollection.CollectionChanged += Comment_CollectionChanged;
+                    foreach (var child in originalCollection)
+                    {
+                        VisitAddChildren(child);
+                    }
+                }
+            }
 
-			if (vm is CommentViewModel)
-			{
-				var comment = vm as CommentViewModel;
-				if (comment.Replies != null)
-				{
-					comment.Replies.CollectionChanged += Comment_CollectionChanged;
-					foreach (ViewModelBase child in comment.Replies)
-					{
-						VisitAddChildren(child, index < 0 ? -1 : index + 1);
-					}
-				}
-			}
-		}
+            private CommentViewModelCollection _originalCollection;
 
-		private void Comment_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-		{
-			if (e.Action == NotifyCollectionChangedAction.Add)
-			{
-				if (sender == originalCollection)
-				{
-					foreach (ViewModelBase vm in e.NewItems)
-					{
-						VisitAddChildren(vm, Comments.Count);
-					}
-				}
-				else
-				{
-					int index = 0;
-					ObservableCollection<ViewModelBase> collection = sender as ObservableCollection<ViewModelBase>;
+            private void VisitAddChildren(ViewModelBase vm, int index = -1)
+            {
+                if (index < 0)
+                    this.Add(vm);
+                else
+                    this.Insert(index, vm);
 
-					// Find the previous element of the triggering collection
-					CommentViewModel previousItem = null;
+                if (vm is CommentViewModel)
+                {
+                    var comment = vm as CommentViewModel;
+                    if (comment.Replies != null)
+                    {
+                        comment.Replies.CollectionChanged += Comment_CollectionChanged;
+                        foreach (ViewModelBase child in comment.Replies)
+                        {
+                            VisitAddChildren(child, index < 0 ? -1 : index + 1);
+                        }
+                    }
+                }
+            }
 
-					if (collection.Count > 1)
-						previousItem = collection[collection.Count - 2] as CommentViewModel;
+            private void Comment_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                {
+                    if (sender == _originalCollection)
+                    {
+                        foreach (ViewModelBase vm in e.NewItems)
+                        {
+                            VisitAddChildren(vm, this.Count);
+                        }
+                    }
+                    else
+                    {
+                        int index = 0;
+                        ObservableCollection<ViewModelBase> collection = sender as ObservableCollection<ViewModelBase>;
 
-					if (previousItem != null)
-					{
-						// If we have the previous item, find its last child
-						var lastChild = GetLastChild(previousItem);
-						index = Comments.IndexOf(lastChild);
-					}
-					else
-					{
-						// Otherwise, use our parent's index
-						for (int i = Comments.Count - 1; i > 0; i--)
-						{
-							if (Comments[i] is CommentViewModel)
-							{
-								var comment = Comments[i] as CommentViewModel;
-								if (comment.Replies == sender)
-								{
-									index = i;
-									break;
-								}
-							}
-						}
-					}
+                        // Find the previous element of the triggering collection
+                        CommentViewModel previousItem = null;
 
-					if (index > 0)
-					{
-						foreach (ViewModelBase vm in e.NewItems)
-						{
-							VisitAddChildren(vm, index + 1);
-						}
-					}
-				}
-			}
-			else if (e.Action == NotifyCollectionChangedAction.Remove)
-			{
-				foreach (ViewModelBase oldItem in e.OldItems)
-				{
-					Comments.Remove(oldItem);
-				}
-			}
-		}
+                        if (collection.Count > 1)
+                            previousItem = collection[collection.Count - 2] as CommentViewModel;
 
-		public CommentViewModel GetLastChild(CommentViewModel vm)
-		{
-			if (vm.Replies == null || vm.Replies.Count == 0)
-				return vm;
+                        if (previousItem != null)
+                        {
+                            // If we have the previous item, find its last child
+                            var lastChild = GetLastChild(previousItem);
+                            index = this.IndexOf(lastChild);
+                        }
+                        else
+                        {
+                            // Otherwise, use our parent's index
+                            for (int i = this.Count - 1; i > 0; i--)
+                            {
+                                if (this[i] is CommentViewModel)
+                                {
+                                    var comment = this[i] as CommentViewModel;
+                                    if (comment.Replies == sender)
+                                    {
+                                        index = i;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
 
-			CommentViewModel lastChild = vm.Replies[vm.Replies.Count - 1] as CommentViewModel;
-			return GetLastChild(lastChild);
-		}
+                        if (index > 0)
+                        {
+                            foreach (ViewModelBase vm in e.NewItems)
+                            {
+                                VisitAddChildren(vm, index + 1);
+                            }
+                        }
+                    }
+                }
+                else if (e.Action == NotifyCollectionChangedAction.Remove)
+                {
+                    foreach (ViewModelBase oldItem in e.OldItems)
+                    {
+                        this.Remove(oldItem);
+                    }
+                }
+            }
+
+            public CommentViewModel GetLastChild(CommentViewModel vm)
+            {
+                if (vm.Replies == null || vm.Replies.Count == 0)
+                    return vm;
+
+                CommentViewModel lastChild = vm.Replies[vm.Replies.Count - 1] as CommentViewModel;
+                return GetLastChild(lastChild);
+            }
+        }
+
+
+		
 
 		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
