84c7bfc549699fc0d7d96bc58ee84807d8957707
Jeff Greene <hippiehunterenator@gmail.com>
2013-07-16 09:30:45 -0700

    background task knows how to set the lockscreen now

--- BaconographyWP8/BackgroundTask.cs
+++ BaconographyWP8/BackgroundTask.cs
@@ -12,6 +12,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media.Imaging;
 
 namespace BaconographyWP8
@@ -331,9 +332,23 @@ namespace BaconographyWP8
 
             var lockScreenView = new LockScreen();
             WriteableBitmap bitmap = new WriteableBitmap(lockScreenView, null);
-            var lockscreenJpg = File.Create(Windows.Storage.ApplicationData.Current.LocalFolder.Path + "\\lockscreen.jpg", 4096);
+            string targetFilePath = Windows.Storage.ApplicationData.Current.LocalFolder.Path + "\\lockscreen.jpg";
+            if (File.Exists(targetFilePath))
+            {
+                targetFilePath = Windows.Storage.ApplicationData.Current.LocalFolder.Path + "\\lockscreenAlt.jpg";
+            }
+            var lockscreenJpg = File.Create(targetFilePath, 4096);
             bitmap.SaveJpeg(lockscreenJpg, bitmap.PixelWidth, bitmap.PixelHeight, 0, 100);
-            
+            LockHelper(targetFilePath, false);
+
+            if (targetFilePath.EndsWith("lockscreenAlt.jpg") && File.Exists(Windows.Storage.ApplicationData.Current.LocalFolder.Path + "\\lockscreen.jpg"))
+            {
+                File.Delete(Windows.Storage.ApplicationData.Current.LocalFolder.Path + "\\lockscreen.jpg");
+            }
+            else if (targetFilePath.EndsWith("lockscreen.jpg") && File.Exists(Windows.Storage.ApplicationData.Current.LocalFolder.Path + "\\lockscreenAlt.jpg"))
+            {
+                File.Delete(Windows.Storage.ApplicationData.Current.LocalFolder.Path + "\\lockscreenAlt.jpg");
+            }
 
             //DONE
             NotifyComplete();
@@ -341,6 +356,49 @@ namespace BaconographyWP8
 
         const string ReadMailGlyph = "\uE166";
         const string UnreadMailGlyph = "\uE119";
+
+        public static async void LockHelper(string filePathOfTheImage, bool isAppResource)
+        {
+            try
+            {
+                var isProvider = Windows.Phone.System.UserProfile.LockScreenManager.IsProvidedByCurrentApplication;
+                if (!isProvider)
+                {
+                    // If you're not the provider, this call will prompt the user for permission.
+                    // Calling RequestAccessAsync from a background agent is not allowed.
+                    var op = await Windows.Phone.System.UserProfile.LockScreenManager.RequestAccessAsync();
+
+                    // Only do further work if the access was granted.
+                    isProvider = op == Windows.Phone.System.UserProfile.LockScreenRequestResult.Granted;
+                }
+
+                if (isProvider)
+                {
+                    // At this stage, the app is the active lock screen background provider.
+
+                    // The following code example shows the new URI schema.
+                    // ms-appdata points to the root of the local app data folder.
+                    // ms-appx points to the Local app install folder, to reference resources bundled in the XAP package.
+                    var schema = isAppResource ? "ms-appx:///" : "ms-appdata:///Local/";
+                    var uri = new Uri(schema + filePathOfTheImage, UriKind.Absolute);
+
+                    // Set the lock screen background image.
+                    Windows.Phone.System.UserProfile.LockScreen.SetImageUri(uri);
+
+                    // Get the URI of the lock screen background image.
+                    var currentImage = Windows.Phone.System.UserProfile.LockScreen.GetImageUri();
+                    System.Diagnostics.Debug.WriteLine("The new lock screen background image is set to {0}", currentImage.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("You said no, so I can't update your background.");
+                }
+            }
+            catch (System.Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+        }
 
         public static async Task MakeLockScreenControl(ISettingsService settingsService, IRedditService redditService, IUserService userService, IImagesService imagesService, IBaconProvider baconProvider)
         {
